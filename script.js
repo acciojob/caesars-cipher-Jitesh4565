3cceeb23ae116803f40454a5aee04fbd70a088f6
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-09-23 11:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,8 +34,11 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	return encodedStr.replace(/[A-Z]/g, function(char){
+		return lookup[char];
+	});
 
-  return; //return decodedArr
+    //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
